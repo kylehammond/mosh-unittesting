2ab837eab8e6b54e8d10875c65a3195b28346eae
Kyle Hammond <kyle.hammond@gmail.com>
2018-04-06 15:56:21 -0500

    Everything up to first tests with Reservations

---
+++ TestNinja/TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
