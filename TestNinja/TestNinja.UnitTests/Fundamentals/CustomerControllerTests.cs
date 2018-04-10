aac099de93232f643c8040da7fb0829f94e3a60d
Kyle Hammond <kyle.hammond@gmail.com>
2018-04-10 07:53:36 -0500

    Stopping point

---
+++ TestNinja/TestNinja.UnitTests/Fundamentals/CustomerControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace TestNinja.UnitTests.Fundamentals
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+    }
+}
